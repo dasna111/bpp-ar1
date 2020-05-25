7a8c459a0ac27933007ff59e2bdeae4135927956
dasna111 <56249201+dasna111@users.noreply.github.com>
2020-05-25 13:50:35 +0200

    first

---
+++ Assets/Game/Chess.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class Chess : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    GameObject ARCam;
+
+    ARSessionOrigin m_SessionOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_SessionOrigin = gameObject.Find("AR Session Origin").GetComponent<ARSessionOrigin>();
+        ARCam = m_SessionOrigin.transform.Find("AR Camera").gameObject;
+        transform.parent = ARCam.transform;
+        NewChess();
+    }
+
+    void NewChess()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+            ChessMove();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            CheckPosition();
+        }
+    }
+}
---
+++ Assets/Game/Main.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class Main : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_BoardPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedBoard
+    {
+        get { return m_BoardPrefab; }
+        set { m_BoardPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedBoard { get; private set; }
+    //////---------------------------------------------------------------------------------
+
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_RookPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedRook
+    {
+        get { return m_RookPrefab; }
+        set { m_RookPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedRook { get; private set; }
+    ///
+    /// -------------------------------------------------------------------
+    ///
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_QueenPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedQueen
+    {
+        get { return m_QueenPrefab; }
+        set { m_QueenPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedQueen { get; private set; }
+
+    /// -----------------------------------------------------------------------
+
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PawnPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPawn
+    {
+        get { return m_PawnPrefab; }
+        set { m_PawnPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedPawn { get; private set; }
+
+    /// ----------------------------------------------------------------------
+
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_KingPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedKing
+    {
+        get { return m_KingPrefab; }
+        set { m_KingPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedKing { get; private set; }
+
+    /// ----------------------------------------------------------------------
+    ///
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_KnightPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedKnight
+    {
+        get { return m_KnightPrefab; }
+        set { m_KnightPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedKnight { get; private set; }
+
+    /// ----------------------------------------------------------------------
+    ///
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_BishopPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedBishop
+    {
+        get { return m_BishopPrefab; }
+        set { m_BishopPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedBishop { get; private set; }
+
+    /// ----------------------------------------------------------------------
+
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    private bool finished = false;
+    private bool isPlaced = false;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (finished)
+            return;
+        if (isPlaced)
+            return;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    spawnedBoard = Instantiate(m_BoardPrefab, hitPose.position, Quaternion.AngleAxis(180, Vector3.up));
+                    spawnedBoard.transform.parent = transform.parent;
+
+                    isPlaced = true;
+
+                    spawnedBishop = Instantiate(m_BishopPrefab);
+                    spawnedBishop.transform.parent = m_RaycastManager.transform.Find("AR Camera").gameObject.transform;
+
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+}
